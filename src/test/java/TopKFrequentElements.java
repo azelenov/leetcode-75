e710ca70d5741995cc7fec966d8a88f9e19686e6
Alex Zelenov <alex.hotwire@gmail.com>
2021-11-24 10:58:18 -0800

    Add solutions for TopKFrequentElements.

---
+++ src/test/java/TopKFrequentElements.java
@@ -0,0 +1,82 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+//Given an integer array nums and an integer k,
+// return the k most frequent elements. You may return the answer in any order.
+public class TopKFrequentElements {
+
+    @Test
+    public void test1() {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] expected = {1, 2};
+        Assert.assertEquals(findTopFreqWithList(nums, k), expected);
+    }
+
+    @Test
+    public void test2() {
+        int[] nums = {1};
+        int k = 1;
+        int[] expected = {1};
+        Assert.assertEquals(findTopFreqWithList(nums, k), expected);
+    }
+
+    @Test
+    public void test3() {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] expected = {1, 2};
+        Assert.assertEquals(findTopFreqWithHeap(nums, k), expected);
+    }
+
+
+//    Runtime: 9 ms, faster than 86.78% of Java online submissions for Top K Frequent Elements.
+//    Memory Usage: 41.5 MB, less than 78.09% of Java online submissions for Top K Frequent Elements.
+    private int[] findTopFreqWithHeap(int[] nums, int top) {
+        if (nums.length == 1) {
+            return nums;
+        }
+        int[] result = new int[top];
+        Map<Integer, Integer> freq = getFreqMap(nums);
+
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(
+                (o1, o2) -> o2.getValue() - o1.getValue());
+        maxHeap.addAll(freq.entrySet());
+        for (int i = 0; i < top; i++) {
+            result[i] = Objects.requireNonNull(maxHeap.poll()).getKey();
+        }
+        return result;
+    }
+
+    private int[] findTopFreqWithList(int[] nums, int top) {
+        if (nums.length == 1) {
+            return nums;
+        }
+        int[] result = new int[top];
+        Map<Integer, Integer> freq = getFreqMap(nums);
+        List<Map.Entry<Integer, Integer>> order = new ArrayList<>(freq.entrySet());
+        System.out.println(freq);
+        order.sort((o1, o2) -> {
+            //sort in reverse
+            return o2.getValue() - o1.getValue();
+        });
+        System.out.println(order);
+
+        for (int i = 0; i < top; i++) {
+            result[i] = order.get(i).getKey();
+        }
+        return result;
+    }
+
+    private Map<Integer, Integer> getFreqMap(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        return freq;
+    }
+
+
+}
